bb56cac241cff142a504b7389d381fea7648b3ed
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-06-19 22:59:56 +0300

    Create register page functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,4 +9,47 @@ router.get('/register', (req, res) => {
     })
 });
 
+router.post('/register',
+    body('username')
+        .isLength({ min: 5 }).withMessage('Username must be at least 5 characters long!')
+        .isAlphanumeric().withMessage('Username may contain only letters and numbers!'),
+    body('password')
+        .isLength({ min: 5 }).withMessage('Password must be at least 5 characters long!')
+        .isAlphanumeric().withMessage('Password may contain only letters and numbers!'),
+    async (req, res) => {
+        try {
+            const { errors } = validationResult(req); //изважда грешките от валидацията на express
+
+            if (errors.length > 0) {  // ако има грещки ги обработва
+                throw errors;
+            }
+
+            if (req.body.username == '' || req.body.password == '') {
+                throw new Error('All fields are required!');
+            }
+
+            if (req.body.password != req.body.repass) {
+                throw new Error('Passwords don\'t match!');
+            }
+            //TODO check if register creates session 
+            const token = await register(req.body.username, req.body.password);
+
+            res.cookie('token', token);
+
+            res.redirect('/');  //TODO replace with redirect by assignment
+        } catch (err) {
+
+            const errors = parseError(err);
+
+            //TODO Add error mesage to actual template 
+            res.render('register', {
+                title: "Register Page",
+                err,
+                body: {
+                    username: req.body.username
+                }
+            })
+        }
+    });
+
 module.exports = router;
